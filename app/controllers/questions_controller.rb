05f88490d81c8e0bab8388f0a1ac1ba0dc83f27e
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-02-20 12:22:02 +0100

    Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,13 @@
 class QuestionsController < ApplicationController
-  def ask
-  end
+  def ask; end
 
   def answer
-    @answer = ''
-    if params[:question] == 'I am going to work right now'
-      @answer = 'Great!'
-    elsif params[:question].end_with?('?')
-      @answer = 'Silly question, get dressed and go to work!'
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
+    @answer = if params[:question] == 'I am going to work right now'
+                'Great!'
+              elsif params[:question].end_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
   end
 end
